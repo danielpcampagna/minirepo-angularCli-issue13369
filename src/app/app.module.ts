46c03e143877bd40161207adaa94f790da5871ab
danielpcampagna <danielprett@gmail.com>
2019-01-08 16:20:58 -0200

    add JIT compiler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, COMPILER_OPTIONS, CompilerFactory, Compiler } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
+
+export function createCompiler(compilerFactory: CompilerFactory) {
+  return compilerFactory.createCompiler();
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: COMPILER_OPTIONS, useValue: {}, multi: true},
+    {provide: CompilerFactory, useClass: JitCompilerFactory, deps: [COMPILER_OPTIONS]},
+    {provide: Compiler, useFactory: createCompiler, deps: [CompilerFactory]},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
